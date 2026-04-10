84f707aa55940d0d2e89617069f8c0e23d3c499b
Nabaya Nestroy <nestroynabaya@gmail.com>
2026-04-10 11:55:37 +0300

    updated

--- app/src/main/java/com/ndejje/momocalc/MainActivity.kt
+++ app/src/main/java/com/ndejje/momocalc/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface {
+            MaterialTheme(typography = MoMoTypography) {
+                Surface(modifier = Modifier.fillMaxSize()) {
                     MoMoCalcScreen()
                 }
             }
